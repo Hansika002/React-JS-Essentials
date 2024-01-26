8a0cec9d00f153fa480e4e8c45d90c99746f7f5f
hansika <hansika.amrutkar@gmail.com>
2024-01-27 01:54:36 +0530

    click event

--- src/Content.js
+++ src/Content.js
@@ -4,11 +4,23 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3);
         return names[int];
       }
+      
+      /*added*/
+      const handleClick = () => {
+        console.log('You clicked it')
+      }
+
+      const handleClick2 = (name) => {
+        console.log('${name} was clicked')
+      }
+
     return (
         <main>
             <p>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click It</button>
+            <button onClick={() => handleClick2('Sam')}>Click It</button>
         </main>
     )
 }
